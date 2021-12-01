41f13b475f6f041e58161cac92a3c58db44ec1df
christoph.baudson <christoph.baudson@rewe-digital.com>
2021-12-01 15:55:45 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.zipWithNext().count { (previousDepthMeasurement, depthMeasurement) ->
+            previousDepthMeasurement < depthMeasurement
+        }
+
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }
+            .windowed(3)
+            .zipWithNext()
+            .map { (previousDepthMeasurementWindow, depthMeasurementWindow) ->
+                previousDepthMeasurementWindow.sum() to
+                depthMeasurementWindow.sum()
+            }.count { (previousDepthMeasurement, depthMeasurement) ->
+                previousDepthMeasurement < depthMeasurement
+            }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
