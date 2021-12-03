76c29713d61ec926997c5f24cefa15f7808e95a0
christoph.baudson <christoph.baudson@rewe-digital.com>
2021-12-03 20:09:53 +0100

    Day 3 alternative with value class

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,73 +1,66 @@
 typealias BinaryNumber = String
-typealias DiagnosticReport = List<BinaryNumber>
-
-fun DiagnosticReport.countOnesAtPosition(position: Int) = count { binaryNumber -> binaryNumber[position] == '1' }
 fun BinaryNumber.toDecimal() = toInt(2)
 
-fun DiagnosticReport.mostCommonBitAt(position: Int): Char =
-    countOnesAtPosition(position).let { numberOfOnes ->
-        when {
-            numberOfOnes >= size - numberOfOnes -> '1'
-            else -> '0'
+typealias Pos = Int
+
+@JvmInline
+value class DiagnosticReport(private val binaryNumbers: List<BinaryNumber>) {
+
+    fun powerConsumption(): Int = gammaRate() * epsilonRate()
+    fun lifeSupportRating(): Int = oxygenGeneratorRating() * co2ScrubberRating()
+
+    private fun countOnesAt(i: Pos) = binaryNumbers.count { binaryNumber -> binaryNumber[i] == '1' }
+
+    private fun mostCommonBitAt(i: Pos): Char =
+        countOnesAt(i).let { numberOfOnes -> if(numberOfOnes >= binaryNumbers.size - numberOfOnes) '1' else '0' }
+
+    private fun leastCommonBitAt(i: Pos): Char = if(mostCommonBitAt(i) == '1') '0' else '1'
+
+    private fun binaryNumberLength() = binaryNumbers.maxOf { it.length }
+
+    private fun gammaRate(): Int =
+        (0 until binaryNumberLength()).map { mostCommonBitAt(it) }.joinToString("").toDecimal()
+
+    private fun epsilonRate(): Int =
+        (0 until binaryNumberLength()).map { leastCommonBitAt(it) }.joinToString("").toDecimal()
+
+    private fun toDecimal(): Int = binaryNumbers.first().toDecimal()
+
+    private fun oxygenGeneratorRatingFilter(i: Pos = 0): DiagnosticReport =
+        binaryNumbers.filter { binaryNumber ->
+            binaryNumber[i] == mostCommonBitAt(i)
+        }.let { filteredReport ->
+            if (filteredReport.size == 1) {
+                DiagnosticReport(filteredReport)
+            } else {
+                DiagnosticReport(filteredReport).oxygenGeneratorRatingFilter(i + 1)
+            }
         }
-    }
 
-fun DiagnosticReport.leastCommonBitAt(position: Int): Char =
-    countOnesAtPosition(position).let { numberOfOnes ->
-        when {
-            numberOfOnes < size - numberOfOnes -> '1'
-            else -> '0'
+    private fun co2ScrubberRatingFilter(i: Pos = 0): DiagnosticReport =
+        binaryNumbers.filter { binaryNumber ->
+            binaryNumber[i] == leastCommonBitAt(i)
+        }.let { filteredReport ->
+            if (filteredReport.size == 1) {
+                DiagnosticReport(filteredReport)
+            } else {
+                DiagnosticReport(filteredReport).co2ScrubberRatingFilter(i + 1)
+            }
         }
-    }
 
-fun DiagnosticReport.numberOfDigits() = maxOf { it.length }
+    private fun oxygenGeneratorRating() = oxygenGeneratorRatingFilter().toDecimal()
+    private fun co2ScrubberRating() = co2ScrubberRatingFilter().toDecimal()
+}
 
-fun DiagnosticReport.gammaRate(): Int =
-    (0 until numberOfDigits()).map { position ->
-        mostCommonBitAt(position)
-    }.joinToString("").toDecimal()
-
-fun DiagnosticReport.epsilonRate(): Int =
-    (0 until numberOfDigits()).map { position ->
-        leastCommonBitAt(position)
-    }.joinToString("").toDecimal()
-
-fun DiagnosticReport.powerConsumption(): Int = gammaRate() * epsilonRate()
-
-fun DiagnosticReport.oxygenGeneratorRatingFilter(position: Int = 0): DiagnosticReport =
-    filter { binaryNumber ->
-        binaryNumber[position] == mostCommonBitAt(position)
-    }.let { filteredReport ->
-        if (filteredReport.size == 1) {
-            filteredReport
-        } else {
-            filteredReport.oxygenGeneratorRatingFilter(position + 1)
-        }
-    }
-
-fun DiagnosticReport.co2ScrubberRatingFilter(position: Int = 0): DiagnosticReport =
-    filter { binaryNumber ->
-        binaryNumber[position] == leastCommonBitAt(position)
-    }.let { filteredReport ->
-        if (filteredReport.size == 1) {
-            filteredReport
-        } else {
-            filteredReport.co2ScrubberRatingFilter(position + 1)
-        }
-    }
-
-fun DiagnosticReport.oxygenGeneratorRating() = oxygenGeneratorRatingFilter().first().toDecimal()
-fun DiagnosticReport.co2ScrubberRating() = co2ScrubberRatingFilter().first().toDecimal()
-fun DiagnosticReport.lifeSupportRating(): Int = oxygenGeneratorRating() * co2ScrubberRating()
 
 fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(testInput.powerConsumption() == 198)
+    check(DiagnosticReport(testInput).powerConsumption() == 198)
 
     val input = readInput("Day03")
-    println(input.powerConsumption())
+    println(DiagnosticReport(input).powerConsumption())
 
-    check(testInput.lifeSupportRating() == 230)
-    println(input.lifeSupportRating())
+    check(DiagnosticReport(testInput).lifeSupportRating() == 230)
+    println(DiagnosticReport(input).lifeSupportRating())
 }
