cb8e73c1971b1e45479ec5edbf616a1bfcf469f7
christoph.baudson <christoph.baudson@rewe-digital.com>
2021-12-08 22:28:52 +0100

    Day 8 after cleanup

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,96 +1,86 @@
+import NoteEntry.Companion.entries
 import kotlin.system.measureTimeMillis
 
-val map = mapOf(
-    "abcefg" to 0, // (6)
-    "cf" to 1, // 2
-    "acdeg" to 2, // (5)
-    "acdfg" to 3, // (5)
-    "bcdf" to 4, // 4
-    "abdfg" to 5, // (5)
-    "abdefg" to 6, // (6)
-    "acf" to 7, // 3
-    "abcdefg" to 8, //7
-    "abcdfg" to 9 // (6)
+typealias Segment = Char
+typealias SignalPattern = Set<Segment>
+typealias Output = Set<Segment>
+
+val sevenSegmentDisplay: Map<Int, Set<Segment>> = mapOf(
+    0 to setOf('a', 'b', 'c', 'e', 'f', 'g'),
+    1 to setOf('c', 'f'),
+    2 to setOf('a', 'c', 'd', 'e', 'g'),
+    3 to setOf('a', 'c', 'd', 'f', 'g'),
+    4 to setOf('b', 'c', 'd', 'f'),
+    5 to setOf('a', 'b', 'd', 'f', 'g'),
+    6 to setOf('a', 'b', 'd', 'e', 'f', 'g'),
+    7 to setOf('a', 'c', 'f'),
+    8 to setOf('a', 'b', 'c', 'd', 'e', 'f', 'g'),
+    9 to setOf('a', 'b', 'c', 'd', 'f', 'g')
 )
 
+class Solution(private val signalPatterns: List<SignalPattern>) {
+
+    private val segmentsForOne = signalPatterns.first { it.size == sevenSegmentDisplay[1]!!.size }
+    private val segmentsForFour = signalPatterns.first { it.size == sevenSegmentDisplay[4]!!.size }
+    private val segmentsForSeven = signalPatterns.first { it.size == sevenSegmentDisplay[7]!!.size }
+
+    private val allSegments = ('a'..'g').toSet()
+    private val segmentB = setOf(allSegments.first { s -> signalPatterns.count { it.contains(s) } == 6 })
+    private val segmentE = setOf(allSegments.first { s -> signalPatterns.count { it.contains(s) } == 4 })
+    private val segmentF = setOf(allSegments.first { s -> signalPatterns.count { it.contains(s) } == 9 })
+
+    private val segmentA = segmentsForSeven - segmentsForOne
+    private val segmentC = segmentsForOne - segmentF
+    private val segmentD = segmentsForFour - segmentC - segmentB - segmentF
+
+    private fun decodeDigit(encodedDigit: Output): Set<Char> =
+        encodedDigit.map { segment ->
+            when (setOf(segment)) {
+                segmentA -> 'a'
+                segmentB -> 'b'
+                segmentC -> 'c'
+                segmentD -> 'd'
+                segmentE -> 'e'
+                segmentF -> 'f'
+                else -> 'g'
+            }
+        }.toSet()
+
+    fun decode(outputValues: List<Output>) = outputValues.map { encodedDigit ->
+        sevenSegmentDisplay.entries.first { (_, segmentsForDigit) ->
+            segmentsForDigit == decodeDigit(encodedDigit)
+        }.key
+    }
+}
+
+data class NoteEntry(
+    val signalPatterns: List<SignalPattern>,
+    val outputValues: List<Output>
+) {
+    companion object {
+        fun entries(input: List<String>): List<NoteEntry> = input.map {
+            it.split(" | ").let { (signals, output) ->
+                NoteEntry(
+                    signalPatterns = signals.split(" ").map(String::toSet),
+                    outputValues = output.split(" ").map(String::toSet)
+                )
+            }
+        }
+    }
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        val signalsAndOutputs = input.map {
-            it.split(" | ").let { (signals, output) ->
-                signals.split(" ") to output.split(" ")
-            }
+    fun part1(input: List<String>) =
+        entries(input).sumOf { (_, outputValues) ->
+            outputValues.count { it.size in setOf(2, 3, 4, 7) }
         }
-        return signalsAndOutputs.sumOf { it.second.count { it.length == 2 || it.length == 3 || it.length == 4 || it.length == 7 } }
 
-    }
-
-    fun String.removeWire(wire: String?): String {
-        return toSet().minus(wire!!.toSet()).joinToString("")
-    }
-
-    fun part2(input: List<String>): Int {
-        val signalsAndOutputs = input.map {
-            it.split(" | ").let { (signals, output) ->
-                signals.split(" ") to output.split(" ")
-            }
+    fun part2(input: List<String>) =
+        entries(input).sumOf { (signalPatterns, outputValues) ->
+            with(Solution(signalPatterns)) {
+                decode(outputValues)
+            }.joinToString("").toInt()
         }
-        return signalsAndOutputs.map { (signals, outputs) ->
-            val possibilities = mutableMapOf<Char, String>(
-                'a' to "abcdefg",
-                'b' to "abcdefg",
-                'c' to "abcdefg",
-                'd' to "abcdefg",
-                'e' to "abcdefg",
-                'f' to "abcdefg",
-                'g' to "abcdefg"
-            )
-
-            // options for c / f
-            val one = signals.find { it.length == 2 }!!
-            possibilities['c'] = one.toList().joinToString("")
-            possibilities['f'] = one.toList().joinToString("")
-
-            // options for a
-            val seven = signals.find { it.length == 3 }
-            val aWire = seven!!.toSet() - one.toSet()
-            possibilities['a'] = aWire.toList().joinToString("")
-
-            // options for b / d
-            val four = signals.find { it.length == 4 }
-            val bdWires = four!!.toSet() - one.toSet()
-            possibilities['b'] = bdWires.toList().joinToString("")
-            possibilities['d'] = bdWires.toList().joinToString("")
-
-            // options for e / g
-            possibilities['e'] = possibilities['e']!!.removeWire(possibilities['a']).removeWire(possibilities['c']).removeWire(possibilities['b'])
-            possibilities['g'] = possibilities['g']!!.removeWire(possibilities['a']).removeWire(possibilities['c']).removeWire(possibilities['b'])
-
-            // identify e / g
-            val zeroSixNine = signals.filter { it.length == 6 }
-            assert(zeroSixNine.size == 3)
-            val intersection069 = zeroSixNine[0].toSet().intersect(zeroSixNine[1].toSet()).intersect(zeroSixNine[2].toSet()).joinToString("")
-            possibilities['g'] = intersection069.removeWire(possibilities['c']).removeWire(possibilities['b']).removeWire(possibilities['a'])
-            possibilities['e'] = possibilities['e']!!.removeWire(possibilities['g'])
-
-            // identify b / d
-            val twoThreeFive = signals.filter { it.length == 5 }
-            assert(twoThreeFive.size == 3)
-            val intersection235 = twoThreeFive[0].toSet().intersect(twoThreeFive[1].toSet()).intersect(twoThreeFive[2].toSet()).joinToString("")
-            possibilities['b'] = possibilities['b']!!.removeWire(intersection235)
-            possibilities['d'] = possibilities['d']!!.removeWire(possibilities['b'])
-
-            // identify c / f
-            val f = possibilities.keys.filter { ch -> signals.count { it.contains(ch) } == 9 }.first()
-            possibilities['f'] = f.toString()
-            possibilities['c'] = possibilities['c']!!.removeWire(possibilities['f'])
-
-            val reversedPossibilities = possibilities.entries.associateBy({ it.value.first() }) { it.key }
-
-            val decoded = outputs.map { digit -> digit.map { reversedPossibilities[it] }.sortedBy { it }.joinToString("") }
-            decoded.map { map[it] }.joinToString("").toInt()
-        }.sum()
-
-    }
 
     val testInput = readInput("Day08_test")
     val input = readInput("Day08")
@@ -113,5 +103,5 @@ fun main() {
         solutionPart2 = part2(input)
     }
     println("$solutionPart2 ($msPart2 ms)")
-    check(solutionPart2 == 99053143)
+    check(solutionPart2 == 990964)
 }
