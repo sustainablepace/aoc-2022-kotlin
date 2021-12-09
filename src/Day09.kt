f30e45fb74a5b8d7e33b7359e0b8dc409db03606
christoph.baudson <christoph.baudson@rewe-digital.com>
2021-12-09 17:28:26 +0100

    Day 9 faster

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,45 +1,65 @@
-import kotlin.math.abs
 import kotlin.system.measureTimeMillis
 
 data class P(val x: Int, val y: Int, val v: Int)
 
-data class Cave(val width: Int, val height: Int, val points: Set<P>) {
-    fun neighbours(p: P) = points.filter {
-        abs(it.x - p.x) + abs(it.y - p.y) == 1
-    }.toSet()
+class Cave(val heightMap: Array<Array<Int>>) {
+    private val width = heightMap.maxOf { it.size }
+    private val height = heightMap.size
 
-    fun lowPoints(): Set<P> {
-        return points.filter { p ->
-            neighbours(p).let { n ->
-                n.isNotEmpty() && n.all { p.v < it.v }
-            }
-        }.toSet()
-    }
-}
-
-fun main() {
-    fun part1(input: List<String>): Int {
-        val m = input.mapIndexed { y, p ->
-            p.indices.map { x ->
-                P(x, y, input[y][x].toString().toInt())
-            }
-        }.flatten().toSet()
-
-        val cave = Cave(input.maxOf { it.length }, input.size, m)
-
-        return cave.lowPoints().sumOf {
-            1 + it.v
+    fun neighbours(x: Int, y: Int): Set<P> =
+        mutableSetOf<P>().let { neighbours ->
+            if (x > 0) neighbours.add(P(x - 1, y, heightMap[y][x - 1]))
+            if (y > 0) neighbours.add(P(x, y - 1, heightMap[y - 1][x]))
+            if (x < width - 1) neighbours.add(P(x + 1, y, heightMap[y][x + 1]))
+            if (y < height - 1) neighbours.add(P(x, y + 1, heightMap[y + 1][x]))
+            neighbours
         }
+
+    private operator fun P.compareTo(neighbours: Set<P>): Int {
+        return if (neighbours.isNotEmpty() && neighbours.all { v < it.v }) -1 else 1
     }
 
-    class Basin(lowPoint: P) {
-        val expansion: MutableList<Set<P>> = mutableListOf(setOf(lowPoint))
+    fun lowPoints(): Set<P> =
+        mutableSetOf<P>().let { lowPoints ->
+            heightMap.forEachIndexed { y, row ->
+                row.forEachIndexed { x, value ->
+                    val p = P(x, y, value)
+                    if (p < neighbours(x, y)) {
+                        lowPoints.add(p)
+                    }
+                }
+            }
+            lowPoints
+        }
 
-        fun contains(p: P) = expansion.any { it.contains(p) }
+    fun basins(): Set<Basin> =
+        lowPoints().map {
+            Basin(this, it)
+        }.let { basins ->
+            var size: Int
+            var newSize = 0
+            do {
+                size = newSize
+                newSize = basins.sumOf { basin ->
+                    basin.expand()
+                    basin.size
+                }
+            } while (newSize > size)
+            basins.toSet()
+        }
 
-        fun expand(cave: Cave) {
+    class Basin(private val cave: Cave, lowPoint: P) {
+        val size: Int get() = expansion.sumOf { it.size }
+
+        private val expansion: MutableList<Set<P>> = mutableListOf(setOf(lowPoint))
+
+        fun expand() {
             expansion.last().flatMap { p ->
-                cave.neighbours(p).filter { it.v < 9 && it.v > p.v && !contains(it) }.toSet()
+                cave.neighbours(p.x, p.y)
+                    .filter {
+                        it.v < 9 && it.v > p.v && !contains(it)
+                    }
+                    .toSet()
             }.toSet().let { expanded ->
                 if (expanded.isNotEmpty()) {
                     expansion.add(expanded)
@@ -47,33 +67,31 @@ fun main() {
             }
         }
 
-        val size: Int get() = expansion.sumOf { it.size }
+        private fun contains(p: P) = expansion.any { it.contains(p) }
     }
+}
 
-    fun part2(input: List<String>): Int {
-        val m = input.mapIndexed { y, p ->
-            p.indices.map { x ->
-                P(x, y, input[y][x].toString().toInt())
-            }
-        }.flatten().toSet()
-
-        val cave = Cave(input.maxOf { it.length }, input.size, m)
-
-        val lowPoints = cave.lowPoints()
-        val basins = lowPoints.map {
-            Basin(it)
+fun parse(input: List<String>): Array<Array<Int>> {
+    val map: Array<Array<Int>> = (input.indices).toList().map<Int, Array<Int>> { arrayOf() }.toTypedArray()
+    input.forEachIndexed { y, p ->
+        p.indices.map { x ->
+            input[y][x].toString().toInt()
+        }.let {
+            map[y] = arrayOf(*it.toTypedArray())
         }
-
-        repeat(cave.height) {
-            basins.map {
-                it.expand(cave)
-            }
-        }
-
-        return basins.map {
-            it.size
-        }.sortedDescending().take(3).fold(1) { acc, v -> acc * v }
     }
+    return map
+}
+
+fun main() {
+    fun part1(input: List<String>): Int =
+        Cave(parse(input)).lowPoints().sumOf { 1 + it.v }
+
+    fun part2(input: List<String>): Int =
+        Cave(parse(input)).basins().map { it.size }
+            .sortedDescending()
+            .take(3)
+            .fold(1) { acc, v -> acc * v }
 
     val testInput = readInput("Day09_test")
     val input = readInput("Day09")
