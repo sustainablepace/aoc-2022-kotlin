9e627e4792a9c3a030ba6b8c97385949cabdfc5f
christoph.baudson <christoph.baudson@rewe-digital.com>
2022-12-01 08:17:19 +0100

    Day 1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -2,27 +2,30 @@ package day01
 
 import readInput
 
+typealias Snacks = List<Int>
+
+fun snacks(input: List<String>): List<Snacks> {
+    return input.joinToString(",")
+        .split(",,")
+        .map { elf ->
+            elf.split(",").map { it.toInt() }
+        }
+}
+fun Snacks.calories(): Int = sum()
+fun List<Snacks>.sortedByCalories() = map { it.calories() }.sortedDescending()
+
 fun main() {
     fun part1(input: List<String>): Int =
-        input.map { it.toInt() }
-            .zipWithNext()
-            .count { (previousDepthMeasurement, depthMeasurement) ->
-                previousDepthMeasurement < depthMeasurement
-            }
+        snacks(input).sortedByCalories().first()
 
     fun part2(input: List<String>): Int =
-        input.map { it.toInt() }
-            .windowed(3)
-            .zipWithNext()
-            .map { (previousDepthMeasurementWindow, depthMeasurementWindow) ->
-                previousDepthMeasurementWindow.sum() to
-                        depthMeasurementWindow.sum()
-            }.count { (previousDepthMeasurement, depthMeasurement) ->
-                previousDepthMeasurement < depthMeasurement
-            }
+        snacks(input).sortedByCalories().take(3).sum()
 
     val testInput = readInput("day01/Day01_test")
-    check(part1(testInput) == 7)
+    println(part1(testInput))
+    check(part1(testInput) == 24000)
+    println(part2(testInput))
+    check(part2(testInput) == 45000)
 
     val input = readInput("day01/Day01")
     println(part1(input))
