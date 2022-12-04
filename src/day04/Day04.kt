6cecfd1c014724a38c040b307b67881f14338037
christoph.baudson <christoph.baudson@rewe-digital.com>
2022-12-04 09:11:13 +0100

    Day 4

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -1,17 +1,33 @@
 package day04
 
+import day22.intersect
 import readInput
 
+typealias CleanupAssignmentPair = Pair<IntRange, IntRange>
+
+fun CleanupAssignmentPair.oneFullyContainsTheOther(): Boolean = first.intersect(second).let { it == second || it == first }
+fun CleanupAssignmentPair.overlap(): Boolean = first.intersect(second).count() != 0
+
+fun List<String>.cleanupAssignments(): List<CleanupAssignmentPair> = map { line: String ->
+    line.split(",").map { pair: String ->
+        pair.split("-").let { (elf1, elf2) ->
+            elf1.toInt()..elf2.toInt()
+        }
+    }.let { (range1, range2) ->
+        range1 to range2
+    }
+}
+
 fun main() {
-    fun part1(input: List<String>): Int = TODO()
-    fun part2(input: List<String>): Int = TODO()
+    fun part1(input: List<String>): Int = input.cleanupAssignments().count(CleanupAssignmentPair::oneFullyContainsTheOther)
+    fun part2(input: List<String>): Int = input.cleanupAssignments().count(CleanupAssignmentPair::overlap)
 
     val testInput = readInput("day04/Day04_test")
     val input = readInput("day04/Day04")
 
-    check(part1(testInput).also { println(it) } == TODO())
-    check(part1(input).also { println(it) } == TODO())
+    check(part1(testInput).also { println(it) } == 2)
+    check(part1(input).also { println(it) } == 556)
 
-    check(part2(testInput).also { println(it) } == TODO())
-    check(part2(input).also { println(it) } == TODO())
+    check(part2(testInput).also { println(it) } == 4)
+    check(part2(input).also { println(it) } == 876)
 }
