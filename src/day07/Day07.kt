dc8bd0e3cc0f284c63249c2e87b18ea9a28302ed
christoph.baudson <christoph.baudson@rewe-digital.com>
2022-12-07 07:35:09 +0100

    Day 7

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -1,21 +1,76 @@
 package day07
 
 import readInput
-import java.util.Collections.max
-import java.util.Collections.min
-import kotlin.math.abs
-import kotlin.system.measureTimeMillis
+
+class File(val name: String, val size: Long)
+
+class Directory(
+    val parent: Directory? = null,
+    val name: String,
+    val directories: MutableList<Directory> = mutableListOf(),
+    val files: MutableList<File> = mutableListOf()
+) {
+    fun fileSize(): Long = directories.sumOf { it.fileSize() } + files.sumOf { it.size }
+
+    fun allSubDirectories(): List<Directory> {
+        val l = mutableListOf<Directory>()
+        l.add(this)
+        l.addAll(directories.flatMap { it.allSubDirectories() })
+        return l.toList()
+    }
+
+    fun rootDirectory(): Directory = if (parent !== null) { parent.rootDirectory() } else this
+}
+
+fun List<String>.filesystem(): Directory {
+    var currentDirectory = Directory(null, "/")
+    forEach { line ->
+        if (line.startsWith("$")) {
+            line.substring(2).let {
+                if (it.startsWith("cd")) {
+                    val newDir = it.substring(3)
+                    currentDirectory = if (newDir == "/") {
+                        currentDirectory.rootDirectory()
+                    } else if (newDir == "..") {
+                        currentDirectory.parent!!
+                    } else {
+                        currentDirectory.directories.first { it.name.endsWith("$newDir/") }
+                    }
+                }
+            }
+        } else {
+            line.split(" ").let { (a, b) ->
+                if (a == "dir") {
+                    currentDirectory.directories.add(Directory(parent = currentDirectory, name = currentDirectory.name + b + "/"))
+                } else {
+                    currentDirectory.files.add(File(name = b, size = a.toLong()))
+                }
+            }
+        }
+    }
+    return currentDirectory.rootDirectory()
+}
 
 fun main() {
-    fun part1(input: List<String>): Int = TODO()
-    fun part2(input: List<String>): Int = TODO()
+    fun part1(input: List<String>): Long = input.filesystem().let { root ->
+        root.allSubDirectories().map { it.fileSize() }.filter { it <= 100000 }.sumOf { it }
+    }
+
+    fun part2(input: List<String>): Long = input.filesystem().let { root ->
+        val totalSize = 70000000L
+        val requiredSizeForUpdate = 30000000L
+        val freeSpace = totalSize - root.fileSize()
+        val needed = requiredSizeForUpdate - freeSpace
+
+        root.allSubDirectories().map { it.fileSize() }.sortedByDescending { it }.last { it >= needed }
+    }
 
     val testInput = readInput("day07/Day07_test")
     val input = readInput("day07/Day07")
 
-    check(part1(testInput).also { println(it) } == TODO())
-    check(part1(input).also { println(it) } == TODO())
+    check(part1(testInput).also { println(it) } == 95437L)
+    check(part1(input).also { println(it) } == 1367870L)
 
-    check(part2(testInput).also { println(it) } == TODO())
-    check(part2(input).also { println(it) } == TODO())
+    check(part2(testInput).also { println(it) } == 24933642L)
+    check(part2(input).also { println(it) } == 549173L)
 }
--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -1,112 +1,18 @@
 package day08
 
-import day08.NoteEntry.Companion.entries
 import readInput
 import kotlin.system.measureTimeMillis
 
-typealias Segment = Char
-typealias SignalPattern = Set<Segment>
-typealias EncodedDigit = Set<Segment>
-typealias SevenSegmentDisplay = Map<Int, Set<Segment>>
-
-val sevenSegmentDisplay: SevenSegmentDisplay = mapOf(
-    0 to setOf('a', 'b', 'c', 'e', 'f', 'g'),
-    1 to setOf('c', 'f'),
-    2 to setOf('a', 'c', 'd', 'e', 'g'),
-    3 to setOf('a', 'c', 'd', 'f', 'g'),
-    4 to setOf('b', 'c', 'd', 'f'),
-    5 to setOf('a', 'b', 'd', 'f', 'g'),
-    6 to setOf('a', 'b', 'd', 'e', 'f', 'g'),
-    7 to setOf('a', 'c', 'f'),
-    8 to setOf('a', 'b', 'c', 'd', 'e', 'f', 'g'),
-    9 to setOf('a', 'b', 'c', 'd', 'f', 'g')
-)
-
-fun SevenSegmentDisplay.findDigit(segments: Set<Segment>) = entries.first { it.value == segments }.key
-
-
-class Solution(private val signalPatterns: List<SignalPattern>) {
-
-    private val segmentsForOne = signalPatterns.first { it.size == sevenSegmentDisplay[1]!!.size }
-    private val segmentsForFour = signalPatterns.first { it.size == sevenSegmentDisplay[4]!!.size }
-    private val segmentsForSeven = signalPatterns.first { it.size == sevenSegmentDisplay[7]!!.size }
-
-    private val allSegments = ('a'..'g').toSet()
-    private val segmentB = setOf(allSegments.first { s -> signalPatterns.count { it.contains(s) } == 6 })
-    private val segmentE = setOf(allSegments.first { s -> signalPatterns.count { it.contains(s) } == 4 })
-    private val segmentF = setOf(allSegments.first { s -> signalPatterns.count { it.contains(s) } == 9 })
-
-    private val segmentA = segmentsForSeven - segmentsForOne
-    private val segmentC = segmentsForOne - segmentF
-    private val segmentD = segmentsForFour - segmentC - segmentB - segmentF
-
-    fun decode(encodedDigit: EncodedDigit) =
-        encodedDigit.map { segment ->
-            when (setOf(segment)) {
-                segmentA -> 'a'
-                segmentB -> 'b'
-                segmentC -> 'c'
-                segmentD -> 'd'
-                segmentE -> 'e'
-                segmentF -> 'f'
-                else -> 'g'
-            }
-        }.toSet().let { segments ->
-            sevenSegmentDisplay.findDigit(segments)
-        }
-}
-
-data class NoteEntry(
-    val signalPatterns: List<SignalPattern>,
-    val outputValues: List<EncodedDigit>
-) {
-    companion object {
-        fun entries(input: List<String>): List<NoteEntry> = input.map {
-            it.split(" | ").let { (signals, output) ->
-                NoteEntry(
-                    signalPatterns = signals.split(" ").map(String::toSet),
-                    outputValues = output.split(" ").map(String::toSet)
-                )
-            }
-        }
-    }
-}
-
 fun main() {
-    fun part1(input: List<String>) =
-        entries(input).sumOf { (_, outputValues) ->
-            outputValues.count { it.size in setOf(2, 3, 4, 7) }
-        }
-
-    fun part2(input: List<String>) =
-        entries(input).sumOf { (signalPatterns, outputValues) ->
-            with(Solution(signalPatterns)) {
-                outputValues.map {
-                    decode(it)
-                }.joinToString("").toInt()
-            }
-        }
+    fun part1(input: List<String>): Int = TODO()
+    fun part2(input: List<String>): Int = TODO()
 
     val testInput = readInput("day08/Day08_test")
     val input = readInput("day08/Day08")
 
-    println(part1(testInput))
-    check(part1(testInput) == 26)
+    check(part1(testInput).also { println(it) } == TODO())
+    check(part1(input).also { println(it) } == TODO())
 
-    val solutionPart1: Int
-    val msPart1 = measureTimeMillis {
-        solutionPart1 = part1(input)
-    }
-    println("$solutionPart1 ($msPart1 ms)")
-    check(solutionPart1 == 554)
-
-    println(part2(testInput))
-    check(part2(testInput) == 61229)
-
-    val solutionPart2: Int
-    val msPart2 = measureTimeMillis {
-        solutionPart2 = part2(input)
-    }
-    println("$solutionPart2 ($msPart2 ms)")
-    check(solutionPart2 == 990964)
+    check(part2(testInput).also { println(it) } == TODO())
+    check(part2(input).also { println(it) } == TODO())
 }
