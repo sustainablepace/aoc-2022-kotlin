ceb6c654bc824bc2fd962c61cd167ebc49e4e81c
christoph.baudson <christoph.baudson@rewe-digital.com>
2022-12-17 23:34:24 +0100

    Day 8

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -1,18 +1,63 @@
 package day08
 
 import readInput
-import kotlin.system.measureTimeMillis
+
+typealias Tree = Int
+typealias TreeLocation = Pair<Int, Int>
+typealias Forest = List<List<Tree>>
+
+fun Forest.rotate() = List(first().size) { index -> map { it[index] } }
+fun Forest.countVisibleTrees() = visibleTreesInRows().union(visibleTreesInColumns()).size
+fun Forest.visibleTreesInColumns() = rotate().visibleTreesInRows().map { (row, col) -> col to row }.toSet()
+
+fun List<Tree>.visibleTrees(): Set<Tree> = (1 until size).mapNotNull { index ->
+    index.takeIf { index == take(index + 1).let { treesSoFar -> treesSoFar.indexOf(treesSoFar.max()) } }
+}.toSet()
+
+fun Forest.visibleTreesInRows(): Set<TreeLocation> = flatMapIndexed { row, line ->
+    when (row) {
+        0, size - 1 -> line.indices.map { it to row }.toSet()
+        else -> line.visibleTrees().map { it to row }.union(
+            line.reversed().visibleTrees().map { line.size - 1 - it to row }
+        )
+    }
+}.toSet()
+
+fun List<String>.forest(): Forest = map { it.map { it.toString().toInt() } }
+
+fun List<Tree>.scenicScore() = when {
+    size > 1 -> takeLast(size - 1).indexOfFirst { first() <= it }.takeIf { it != -1 }?.plus(1) ?: (size - 1)
+    else -> 0
+}
+
 
 fun main() {
-    fun part1(input: List<String>): Int = TODO()
-    fun part2(input: List<String>): Int = TODO()
+    fun part1(input: List<String>): Int = input.forest().countVisibleTrees()
+
+    fun part2(input: List<String>): Int {
+        val forest = input.forest()
+        val rotatedForest = forest.rotate()
+
+        return forest.flatMapIndexed { rIndex, row ->
+            List(row.size) { cIndex ->
+                val left = row.take(cIndex + 1).reversed()
+                val right = row.takeLast(row.size - cIndex)
+
+                val column = rotatedForest[cIndex]
+                val up = column.take(rIndex + 1).reversed()
+                val down = column.takeLast(column.size - rIndex)
+
+                left.scenicScore() * right.scenicScore() * up.scenicScore() * down.scenicScore()
+            }
+        }.max()
+    }
 
     val testInput = readInput("day08/Day08_test")
     val input = readInput("day08/Day08")
 
-    check(part1(testInput).also { println(it) } == TODO())
-    check(part1(input).also { println(it) } == TODO())
+    check(part1(testInput).also { println(it) } == 21)
+    check(part1(input).also { println(it) } == 1796)
 
-    check(part2(testInput).also { println(it) } == TODO())
-    check(part2(input).also { println(it) } == TODO())
+    check(part2(testInput).also { println(it) } == 8)
+    check(part2(input).also { println(it) } == 288120)
 }
--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -3,119 +3,16 @@ package day09
 import readInput
 import kotlin.system.measureTimeMillis
 
-data class P(val x: Int, val y: Int, val v: Int)
-
-class Cave_(val heightMap: Array<Array<Int>>) {
-    private val width = heightMap.maxOf { it.size }
-    private val height = heightMap.size
-
-    fun neighbours(x: Int, y: Int): Set<P> =
-        mutableSetOf<P>().let { neighbours ->
-            if (x > 0) neighbours.add(P(x - 1, y, heightMap[y][x - 1]))
-            if (y > 0) neighbours.add(P(x, y - 1, heightMap[y - 1][x]))
-            if (x < width - 1) neighbours.add(P(x + 1, y, heightMap[y][x + 1]))
-            if (y < height - 1) neighbours.add(P(x, y + 1, heightMap[y + 1][x]))
-            neighbours
-        }
-
-    private operator fun P.compareTo(neighbours: Set<P>): Int {
-        return if (neighbours.isNotEmpty() && neighbours.all { v < it.v }) -1 else 1
-    }
-
-    fun lowPoints(): Set<P> =
-        mutableSetOf<P>().let { lowPoints ->
-            heightMap.forEachIndexed { y, row ->
-                row.forEachIndexed { x, value ->
-                    val p = P(x, y, value)
-                    if (p < neighbours(x, y)) {
-                        lowPoints.add(p)
-                    }
-                }
-            }
-            lowPoints
-        }
-
-    fun basins(): Set<Basin> =
-        lowPoints().map {
-            Basin(this, it)
-        }.let { basins ->
-            var size: Int
-            var newSize = 0
-            do {
-                size = newSize
-                newSize = basins.sumOf { basin ->
-                    basin.expand()
-                    basin.size
-                }
-            } while (newSize > size)
-            basins.toSet()
-        }
-
-    class Basin(private val cave: Cave_, lowPoint: P) {
-        val size: Int get() = expansion.sumOf { it.size }
-
-        private val expansion: MutableList<Set<P>> = mutableListOf(setOf(lowPoint))
-
-        fun expand() {
-            expansion.last().flatMap { p ->
-                cave.neighbours(p.x, p.y)
-                    .filter {
-                        it.v < 9 && it.v > p.v && !contains(it)
-                    }
-                    .toSet()
-            }.toSet().let { expanded ->
-                if (expanded.isNotEmpty()) {
-                    expansion.add(expanded)
-                }
-            }
-        }
-
-        private fun contains(p: P) = expansion.any { it.contains(p) }
-    }
-}
-
-fun parse(input: List<String>): Array<Array<Int>> {
-    val map: Array<Array<Int>> = (input.indices).toList().map<Int, Array<Int>> { arrayOf() }.toTypedArray()
-    input.forEachIndexed { y, p ->
-        p.indices.map { x ->
-            input[y][x].toString().toInt()
-        }.let {
-            map[y] = arrayOf(*it.toTypedArray())
-        }
-    }
-    return map
-}
-
 fun main() {
-    fun part1(input: List<String>): Int =
-        Cave_(parse(input)).lowPoints().sumOf { 1 + it.v }
+    fun part1(input: List<String>): Int = TODO()
+    fun part2(input: List<String>): Int = TODO()
 
-    fun part2(input: List<String>): Int =
-        Cave_(parse(input)).basins().map { it.size }
-            .sortedDescending()
-            .take(3)
-            .fold(1) { acc, v -> acc * v }
+    val testInput = readInput("day08/Day08_test")
+    val input = readInput("day08/Day08")
 
-    val testInput = readInput("day09/Day09_test")
-    val input = readInput("day09/Day09")
+    check(part1(testInput).also { println(it) } == TODO())
+    check(part1(input).also { println(it) } == TODO())
 
-    println(part1(testInput))
-    check(part1(testInput) == 15)
-
-    val solutionPart1: Int
-    val msPart1 = measureTimeMillis {
-        solutionPart1 = part1(input)
-    }
-    println("$solutionPart1 ($msPart1 ms)")
-    check(solutionPart1 == 478)
-
-    println(part2(testInput))
-    check(part2(testInput) == 1134)
-
-    val solutionPart2: Int
-    val msPart2 = measureTimeMillis {
-        solutionPart2 = part2(input)
-    }
-    println("$solutionPart2 ($msPart2 ms)")
-    check(solutionPart2 == 1327014)
+    check(part2(testInput).also { println(it) } == TODO())
+    check(part2(input).also { println(it) } == TODO())
 }
