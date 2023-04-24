fb5d6d38efb7bc4c46395c00218748bf30d6c1f1
Christoph Baudson <christoph.baudson@rewe-digital.com>
2023-04-24 21:42:09 +0200

    day 13+14 prep

--- src/day13/Day13.kt
+++ src/day13/Day13.kt
@@ -1,98 +1,33 @@
 package day13
 
 import readInput
-import kotlin.math.abs
 import kotlin.system.measureTimeMillis
 
-data class Dot(val x: Int, val y: Int)
-class FoldInstruction private constructor(val isHorizontalFold: Boolean, val foldLine: Int) {
-    companion object {
-        fun create(input: String): FoldInstruction =
-            input
-                .replace("fold along ", "")
-                .split("=")
-                .let { (orientation, position) ->
-                    FoldInstruction(
-                        isHorizontalFold = orientation.first() == 'y',
-                        foldLine = position.toInt()
-                    )
-                }
-    }
-}
-
-typealias Paper = Set<Dot>
-
-fun Paper.fold(vararg foldInstructions: FoldInstruction): Paper =
-    foldInstructions.fold(this) { foldedPaper, instruction ->
-        if (instruction.isHorizontalFold) {
-            foldedPaper.map { (x, y) ->
-                Dot(
-                    x = x,
-                    y = instruction.foldLine - abs(y - instruction.foldLine)
-                )
-            }.toSet()
-        } else {
-            foldedPaper.map { (x, y) ->
-                Dot(
-                    x = instruction.foldLine - abs(x - instruction.foldLine),
-                    y = y
-                )
-            }.toSet()
-        }
-    }
-
-fun Paper.print() = groupBy { it.y }
-    .toSortedMap()
-    .map { (_, dotsInLine) ->
-        (0..dotsInLine.maxOf { it.x }).map { x ->
-            if (dotsInLine.any { it.x == x }) '#' else ' '
-        }.joinToString("").let {
-            println(it)
-        }
-    }
-
 fun main() {
-    fun parse(input: List<String>): Pair<List<FoldInstruction>, Paper> =
-        input
-            .filter { it.isNotBlank() }
-            .partition { it.startsWith("fold") }
-            .let { (foldInstructions, dots) ->
-                foldInstructions.map {
-                    FoldInstruction.create(it)
-                } to dots.map { dot ->
-                    dot.split(",")
-                        .map { it.toInt() }
-                        .let { (x, y) -> Dot(x, y) }
-                }.toSet()
-            }
+    fun part1(input: List<String>): Int = TODO()
 
-    fun part1(input: List<String>): Int =
-        parse(input).let { (foldInstructions, paper) ->
-            paper.fold(foldInstructions.first()).size
-        }
-
-    fun part2(input: List<String>): Unit =
-        parse(input).let { (foldInstructions, paper) ->
-            paper.fold(*foldInstructions.toTypedArray()).print()
-        }
+    fun part2(input: List<String>): Int = TODO()
 
     val testInput = readInput("day13/Day13_test")
     val input = readInput("day13/Day13")
 
     println(part1(testInput))
-    check(part1(testInput) == 17)
+    check(part1(testInput) == TODO())
 
     val solutionPart1: Int
     val msPart1 = measureTimeMillis {
         solutionPart1 = part1(input)
     }
     println("$solutionPart1 ($msPart1 ms)")
-    check(solutionPart1 == 602)
+    check(solutionPart1 == TODO())
 
-    part2(testInput)
+    println(part2(testInput))
+    check(part2(testInput) == TODO())
 
+    val solutionPart2: Int
     val msPart2 = measureTimeMillis {
-        part2(input)
+        solutionPart2 = part2(input)
     }
-    println("($msPart2 ms)")
+    println("$solutionPart2 ($msPart2 ms)")
+    check(solutionPart2 == TODO())
 }
